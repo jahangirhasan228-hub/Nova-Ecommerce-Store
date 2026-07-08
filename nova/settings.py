493b52b1820c2de46c23c9fb8f141cf30aa0dd82
Films marketing <jahangirhasan228@gmail.com>
2026-07-08 23:09:09 +0600

    Update settings.py

--- nova/settings.py
+++ nova/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 import sys
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,10 +80,10 @@ WSGI_APPLICATION = 'nova.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+    )
 }
 
 
